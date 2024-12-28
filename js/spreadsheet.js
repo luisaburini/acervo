10bc6926df3b092dc65da934968c96b70b09cc04
Luisa Burini <lburini@unicamp.br>
2024-12-28 14:10:11 -0300

    :bug: Corrige bugs

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -26,19 +26,9 @@ hlinks = [
     "https://live.staticflickr.com/65535/54232250160_c844033733_b.jpg"
 ]
 
-
-function onSearched() {
-    var x = document.getElementById("searchbox").value;
-    for (let i=0; i<fields.length;i++) {
-        if (fields[i].toLowerCase().includes(x) || names[i].toLowerCase().includes(x) || 
-        doctypes[i].toLowerCase().includes(x) || authors[i].toLowerCase().includes(x) || 
-        dates[i].toLowerCase().includes(x) || places[i].toLowerCase().includes(x)) {
-            createNewImage(hlinks[i], names[i])
-        }
-    }
-}
-
 function createNewImage(hlink, name) {
+    console.log(hlink);
+    console.log(name);
     var img = IEWIN ? new Image() : document.createElement(hlink);
     img.src = hlink;
     if ( alt != null ) img.alt = name;

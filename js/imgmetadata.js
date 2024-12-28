ae7e3448815315c5c9e98f7a0aa67ce252c33814
Luisa Burini <lburini@unicamp.br>
2024-12-28 15:12:16 -0300

    :bug: conserta busca

--- js/imgmetadata.js
+++ js/imgmetadata.js
@@ -1,3 +1,38 @@
+var fields = [
+    "Filme",
+    "Fotografia"
+]
+
+var names = [
+    "Spectros - algum nome nenhum rosto",
+    "Fazenda da Prefeitura em Guaratiba"
+]
+
+var doctypes = [
+    "Documentário",
+    "Fotografia"
+]
+
+var authors = [
+    "Ciro Lubliner",
+    "Augusto Malta"
+]
+
+var dates = [
+    "20/12/2024",
+    "1/11/1928"
+]
+
+var places = [
+    "Sorocaba, SP, São Paulo",
+    "Fazenda da Prefeitura de Guaratiba, RJ, Rio de Janeiro"
+]
+
+var hlinks = [
+    "https://live.staticflickr.com/65535/54232250160_c844033733_b.jpg",
+    "https://live.staticflickr.com/65535/54232284053_7845082b27_b.jpg",
+]
+
 class ImgMetadata {
     constructor(field, name, doctype, author, date, place, hlink) {
         this.field = field
--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -1,36 +1,67 @@
-fields = [
-    "Filme"
+var fields = [
+    "Filme",
+    "Fotografia"
 ]
 
-names = [
-    "Spectros - algum nome nenhum rosto"
+var names = [
+    "Spectros - algum nome nenhum rosto",
+    "Fazenda da Prefeitura em Guaratiba"
 ]
 
-doctypes = [
-    "Documentário"
+var doctypes = [
+    "Documentário",
+    "Fotografia"
 ]
 
-authors = [
-    "Ciro Lubliner"
+var authors = [
+    "Ciro Lubliner",
+    "Augusto Malta"
 ]
 
-dates = [
-    "20/12/2024"
+var dates = [
+    "20/12/2024",
+    "1/11/1928"
 ]
 
-places = [
-    "Sorocaba, SP, São Paulo"
+var places = [
+    "Sorocaba, SP, São Paulo",
+    "Fazenda da Prefeitura de Guaratiba, RJ, Rio de Janeiro"
 ]
 
-hlinks = [
-    "https://live.staticflickr.com/65535/54232250160_c844033733_b.jpg"
+var hlinks = [
+    "https://live.staticflickr.com/65535/54232250160_c844033733_b.jpg",
+    "https://live.staticflickr.com/65535/54232284053_7845082b27_b.jpg",
 ]
 
-function createNewImage(hlink, name) {
+
+function onSearched() {
+    var grid = document.getElementById('grid-container')
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    var searchBox = document.getElementById("searchbox")
+    var toSearch = searchBox.value;
+    searchBox.value = ""
+    
+    
+    for (let i=0; i<fields.length;i++) {
+        console.log(names[i]);
+        if (fields[i].toLowerCase().includes(toSearch) || names[i].toLowerCase().includes(toSearch) || 
+            doctypes[i].toLowerCase().includes(toSearch) || authors[i].toLowerCase().includes(toSearch) || 
+            dates[i].toLowerCase().includes(toSearch) || places[i].toLowerCase().includes(toSearch)) {
+            console.log("Create new image")
+            createNewImage(hlinks[i], names[i], grid)
+        }
+    }
+ }
+
+function createNewImage(hlink, name, grid) {
     console.log(hlink);
     console.log(name);
-    var img = IEWIN ? new Image() : document.createElement(hlink);
+    let img = document.createElement('img');
     img.src = hlink;
-    if ( alt != null ) img.alt = name;
-    if ( title != null ) img.title = name;
+    img.alt = name;
+    img.title = name;
+    grid.appendChild(img);
 }

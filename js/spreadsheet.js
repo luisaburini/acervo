301c1698b4d8de157847016f29ab8a499fa564eb
Luisa Burini <lburini@unicamp.br>
2025-07-17 22:53:24 -0300

    :under_construction: teste botoes icones zoom

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -495,35 +495,23 @@ function onZoomIn() {
   let container = document.getElementsByClassName("overlayImg")[0],
   imgsrc = container.currentStyle || window.getComputedStyle(container, false);
   imgsrc = imgsrc.backgroundImage.slice(4, -1).replace(/"/g, "");
+  console.log(imgsrc)
    
-// (B) LOAD IMAGE + ATTACH ZOOM
+  // (B) LOAD IMAGE + ATTACH ZOOM
   let img = new Image();
   img.src = imgsrc;
   img.onload = () => {
-// (B1) CALCULATE ZOOM RATIO
-  let ratio = img.naturalHeight / img.naturalWidth,  percentage = ratio * 100 + "%";
-
-// (B2) ATTACH ZOOM ON MOUSE MOVE
-  container.onmousemove = e => {
-    let rect = e.target.getBoundingClientRect(),
-            xPos = e.clientX - rect.left,
-            yPos = e.clientY - rect.top,
-            xPercent = xPos / (container.clientWidth / 100) + "%",
-            yPercent = yPos / ((container.clientWidth * ratio) / 100) + "%";
-    
-      Object.assign(container.style, {
-        backgroundPosition: xPercent + " " + yPercent,
-        backgroundSize: img.naturalWidth + "px"
-      });
-    };
-    
-// (B3) RESET ZOOM ON MOUSE LEAVE
-    container.onmouseleave = e => {
+        // (B1) CALCULATE ZOOM RATIO
+        let ratio = img.naturalHeight / img.naturalWidth,  percentage = ratio * 100 + "%";
+        let rect = e.target.getBoundingClientRect(),
+                xPos = e.clientX - rect.left,
+                yPos = e.clientY - rect.top,
+                xPercent = xPos / (container.clientWidth / 100) + "%",
+                yPercent = yPos / ((container.clientWidth * ratio) / 100) + "%";
         Object.assign(container.style, {
-            backgroundPosition: "center",
-            backgroundSize: "cover"
-        });
-      };
+            backgroundPosition: xPercent + " " + yPercent,
+            backgroundSize: img.naturalWidth + "px"
+        });    
     }
 }
 

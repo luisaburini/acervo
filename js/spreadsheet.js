398eac2d3f4763ced1a63368831db85dc2e3a60d
Luisa Burini <lburini@unicamp.br>
2025-07-17 22:32:35 -0300

    :under_construction: testes com zoom

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -480,3 +480,51 @@ function on(link) {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+
+function goHome() {
+    off();
+}
+
+function onZoomIn() {
+// CREDITS : https://www.cssscript.com/image-zoom-pan-hover-detail-view/
+// (A) GET CONTAINER + IMAGE SOURCE
+  
+  let container = document.getElementsByClassName("overlayImg")[0],
+  imgsrc = container.currentStyle || window.getComputedStyle(container, false);
+  imgsrc = imgsrc.backgroundImage.slice(4, -1).replace(/"/g, "");
+   
+// (B) LOAD IMAGE + ATTACH ZOOM
+  let img = new Image();
+  img.src = imgsrc;
+  img.onload = () => {
+// (B1) CALCULATE ZOOM RATIO
+  let ratio = img.naturalHeight / img.naturalWidth,  percentage = ratio * 100 + "%";
+
+// (B2) ATTACH ZOOM ON MOUSE MOVE
+  container.onmousemove = e => {
+    let rect = e.target.getBoundingClientRect(),
+            xPos = e.clientX - rect.left,
+            yPos = e.clientY - rect.top,
+            xPercent = xPos / (container.clientWidth / 100) + "%",
+            yPercent = yPos / ((container.clientWidth * ratio) / 100) + "%";
+    
+      Object.assign(container.style, {
+        backgroundPosition: xPercent + " " + yPercent,
+        backgroundSize: img.naturalWidth + "px"
+      });
+    };
+    
+// (B3) RESET ZOOM ON MOUSE LEAVE
+    container.onmouseleave = e => {
+        Object.assign(container.style, {
+            backgroundPosition: "center",
+            backgroundSize: "cover"
+        });
+      };
+    }
+}
+
+function onZoomOut() {
+    
+}
+

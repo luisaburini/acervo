08efee7a185e0216e5a60d85f05a5c9ed45679d3
Luisa Burini <lburini@unicamp.br>
2025-10-13 08:01:10 -0300

    :bug: tentativa de consertar endereço das imagens

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -239,7 +239,7 @@ function populateNavegue() {
         decadeButton.innerText = allDecades[i];
         decadeButton.onmouseover = function() {
             decadeButton.innerText = "";
-            imgPath = "url(‘../imgs/Foto_"+allDecades[i]+".jpg‘)"
+            imgPath = "url(‘./imgs/Foto_"+allDecades[i]+".jpg‘)"
             decadeButton.style.backgroundImage = imgPath;
             console.log(allDecades[i]);
             console.log(imgPath);
